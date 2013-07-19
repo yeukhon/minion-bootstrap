8248cb40421cecd45d3dce4678b179fb2d0ae6a1
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-07-19 00:52:53 -0700

    Fixed ``zap`` should not be in the template tag in environment.js

--- templates/environment.js
+++ templates/environment.js
@@ -1,1 +1,1 @@
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:{{MINION_PLUGINS_DIR/zap}}"
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:{{MINION_PLUGINS_DIR}}/zap"
